9fc89e9ad2ed2a55d281aceeeca4d6baf9720158
srslte <borjainestahernandez@hotmail.com>
2020-06-17 15:25:26 +0200

    Less buffer_pool to work with IRIS testbed

--- srsenb/src/enb.cc
+++ srsenb/src/enb.cc
@@ -53,7 +53,8 @@ void enb::cleanup()
   pthread_mutex_unlock(&enb_instance_mutex);
 }
 //ENB_POOL_SIZE == 10240
-enb::enb() : started(false), pool(srslte::byte_buffer_pool::get_instance(500000))
+// 500000 too big for IRIS testbed resources?
+enb::enb() : started(false), pool(srslte::byte_buffer_pool::get_instance(300000))
 {
   // print build info
   std::cout << std::endl << get_build_string() << std::endl;
